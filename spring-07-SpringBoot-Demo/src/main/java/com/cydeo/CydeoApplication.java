c424fc0273fc04a54bc36b34a478eecbe9d8dc61
Baturay Gok <baturay.gok@gmail.com>
2023-03-30 02:58:51 -0500

    Spring Boot demo module created

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -0,0 +1,29 @@
+package com.cydeo;
+
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CydeoApplication {
+
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Baturay");
+        comment.setText("Spring Framework");
+
+         ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
+
+        CommentService commentService = container.getBean(CommentService.class);
+        commentService.publishComment(comment);
+
+    }
+
+
+
+
+}
