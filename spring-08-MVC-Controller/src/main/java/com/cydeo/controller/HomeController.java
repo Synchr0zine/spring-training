a4bda6c403359ec5ee1d92edf954d5408e222ad7
Baturay Gok <baturay.gok@gmail.com>
2023-03-31 16:39:02 -0500

    Added two more controllers and html files

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.cydeo.controller;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -9,6 +9,15 @@ public class HomeController {
 
     @RequestMapping("/home")
     public String home(){
+        return "home.html";
+    }
+
+    @RequestMapping // works at homepage
+    public String home1(){
+        return "home.html";
+    }
+    @RequestMapping ({"/apple","/orange"}) //multiple endpoints
+    public String home2(){
         return "home.html";
     }
 
