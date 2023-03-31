a7927176771189f06f6766d1b358eb12c212ae85
Baturay Gok <baturay.gok@gmail.com>
2023-03-31 17:14:21 -0500

    Added attributes to controller, created thymeleaf template

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/CydeoApp09.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CydeoApp09 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CydeoApp09.class, args);
+	}
+
+}
