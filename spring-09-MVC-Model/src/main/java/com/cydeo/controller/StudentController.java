fb113298f41c39a07d282905bd05e8b9928e8f1a
Baturay Gok <baturay.gok@gmail.com>
2023-04-01 05:19:30 -0500

    Added few more attributes

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -12,6 +18,18 @@ public class StudentController {
 
         model.addAttribute("name","Baturay");
         model.addAttribute("job","Developer");
+
+        String subject = "Spring Boot";
+        model.addAttribute("subject",subject);
+
+        int studentID = new Random().nextInt();
+        model.addAttribute("id",studentID);
+
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(4, 5, 6, 7));
+        model.addAttribute("numbers",numbers);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date",dt);
 
         return "student/welcome";
     }
