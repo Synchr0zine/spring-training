fdb49bcf629f29d8d8c3b748b039f36e888d35d9
Baturay Gok <baturay.gok@gmail.com>
2023-04-02 04:23:28 -0500

    Implemented RequestParam by QueryParameter

---
+++ spring-09-MVC-Model/src/main/java/com/car/controller/CarController.java
@@ -0,0 +1,35 @@
+package com.car.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CarController {
+
+    @RequestMapping("/info")
+    public String carInfo(@RequestParam String make, Model model) {
+
+        model.addAttribute("make", make);
+
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value = "make",required = false,defaultValue = "KIA") String make, Model model) {
+
+        model.addAttribute("make", make);
+
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info3")
+    public String carInfo3(@RequestParam String make,@RequestParam Integer year, Model model) { //localhost:8080/info3?make=Honda&year=2005
+
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+
+        return "car/car-info";
+    }
+}
