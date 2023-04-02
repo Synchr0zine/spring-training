3c89813fb23fe4588c1d156a15e82eba3926d68d
Baturay Gok <baturay.gok@gmail.com>
2023-04-01 22:13:48 -0500

    Added mentor table

---
+++ spring-09-MVC-Model/src/main/java/com/task/MentorApp.java
@@ -0,0 +1,11 @@
+package com.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MentorApp.class,args);
+    }
+}
---
+++ spring-09-MVC-Model/src/main/java/com/task/controller/MentorController.java
@@ -0,0 +1,27 @@
+package com.task.controller;
+
+import com.task.enums.Gender;
+import com.task.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MentorController {
+
+
+    @RequestMapping("/mentor")
+    public String showTable(Model model){
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike","Smith",45, Gender.MALE));
+        mentorList.add(new Mentor("Tom","Hanks",65, Gender.MALE));
+        mentorList.add(new Mentor("Amy","Bryan",25, Gender.FEMALE));
+
+        model.addAttribute("mentors",mentorList);
+        return "mentor/mentor-list";
+    }
+}
+
