d1525e5b120d16a9bfd307dedc5448c0299f6dab
Baturay Gok <baturay.gok@gmail.com>
2023-04-04 06:11:05 -0500

    Updated the registration page

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
     @GetMapping ("/register")
     public String register(Model model) {
+
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
 
         model.addAttribute("mentor",new Mentor());
         return "mentor/mentor-register";
