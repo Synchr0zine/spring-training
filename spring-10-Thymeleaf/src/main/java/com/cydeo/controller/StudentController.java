226f657c01a73d9757bafa0c101455744508b13f
Baturay Gok <baturay.gok@gmail.com>
2023-04-04 05:33:15 -0500

    GetMapping, PostMapping, Mentor register page created

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,20 +3,24 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+    // @RequestMapping("/register")
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+    // @RequestMapping("/welcome")
+    @PostMapping("/welcome")
     public String welcome(){
         return "student/welcome";
     }
